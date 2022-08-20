d51ba4117103de5a9639f445ee7047ba40b2f80c
ferrell-code <automatedcharlesferrell@gmail.com>
2022-08-20 00:16:00 -0400

    working on dex add incentives query

--- src/trace/dex-trace.ts
+++ src/trace/dex-trace.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs'
+import { readFileSync, writeFileSync} from 'fs'
 
 import '@acala-network/types'
 import '@acala-network/type-definitions'
@@ -8,6 +8,8 @@ import { Wallet } from '@acala-network/sdk/wallet'
 import { formatBalance, table } from '../log'
 import runner from '../runner'
 import { assert } from 'console'
+import util from 'util'
+import { Json } from '@polkadot/types-codec'
 
 runner()
   .requiredNetwork(['acala'])
@@ -17,6 +19,13 @@ runner()
     console.log(`
     iBTC trace
     `)
+
+    const beforeBlock = 1638215
+    const afterBlock = 1639493
+    const blockNow = (await apiAt.query.system.number()).toNumber()
+
+    const wallet = new Wallet(api)
+    const stableCurrency = await wallet.getToken(api.consts.cdpEngine.getStableCurrencyId)
 
     const tokenDecimal = {
         'ACA': 12,
@@ -71,37 +80,126 @@ runner()
     const path = __dirname + '/dex-events.json'
 
     const data: any[] = JSON.parse(readFileSync(path, 'utf8'))
-    const sucessful = data.filter((val) => val.result != 0);
+    const successful = data.filter((val) => val.result != 0)
+    const accountList = new Set<string>()
+    for (const info of successful) {
+        if (info.signer != undefined) {
+            accountList.add(info.signer);
+        }
+    }
 
-    const dexTxs = sucessful.filter((val) => val.section == 'dex');
-    const transactionFeeTxs = sucessful.filter((val) => val.section == 'transactionPayment');
-    const aggregatedDexTxs = sucessful.filter((val) => val.section == 'aggregatedDex');
-    const honzonTxs = sucessful.filter((val) => val.section == 'honzon');
-    const cdpEngineTxs = sucessful.filter((val) => val.section == 'cdpEngine');
-    const utilityTxs = sucessful.filter((val) => val.section == 'utility');
-    const democracyTxs = sucessful.filter((val) => val.section == 'democracy');
-    const vestingTxs = sucessful.filter((val) => val.section == 'vesting');
+    const queryData = async (block: number, address: string) => {
+      const apiAt = await api.at(await api.rpc.chain.getBlockHash(block))
+      let ausdData = await apiAt.query.tokens.accounts(address, stableCurrency.toCurrencyId(api));
+      return BigInt(ausdData.free.toString())
+    }
 
+    let accountsAUSD = [] as {who: string, after: bigint, before: bigint, diff: bigint}[];
+    for (const who of accountList) {
+        const beforeAUSD = await queryData(beforeBlock, who);
+        const nowAUSD = await queryData(blockNow, who);
+        const diffAUSD = nowAUSD - beforeAUSD;
+        console.log(diffAUSD)
+        accountsAUSD.push({who: who, after: nowAUSD, before: beforeAUSD, diff: diffAUSD})
+    }
+
+    const totalDiffAUSD = accountsAUSD.reduce((prev, curr) => {
+        return prev + curr.diff
+    }, BigInt(0))
+
+    table(accountsAUSD);
+    console.log("totalAUSD accounted for: ", formatBalance(totalDiffAUSD))
+
+    const json = JSON.stringify(accountsAUSD, null, 2);
+    writeFileSync('ausdAccounts.json', json);
+
+    /*
+    // Clean all calls into their respective modules
+    const dexTxs = successful.filter((val) => val.section == 'dex');
+    const transactionFeeTxs = successful.filter((val) => val.section == 'transactionPayment');
+    const aggregatedDexTxs = successful.filter((val) => val.section == 'aggregatedDex');
+    const honzonTxs = successful.filter((val) => val.section == 'honzon');
+    const cdpEngineTxs = successful.filter((val) => val.section == 'cdpEngine');
+    const utilityTxs = successful.filter((val) => val.section == 'utility');
+    const democracyTxs = successful.filter((val) => val.section == 'democracy');
+    const vestingTxs = successful.filter((val) => val.section == 'vesting');
+
+    //console.log(util.inspect(transactionFeeTxs, {showHidden: false, depth: null, colors: true}))
     const filteredTxs = [dexTxs, transactionFeeTxs, aggregatedDexTxs, honzonTxs, cdpEngineTxs, utilityTxs, democracyTxs, vestingTxs]
 
     let lenSum = 0
     for (const i of filteredTxs) {
-        console.log("section:",i[0].section);
-        console.log(i.length, "\n");
-
+        //console.log("section:",i[0].section);
+        //console.log(i.length, "\n");
         lenSum += i.length;
     }
-    assert(lenSum == sucessful.length);
+    //console.log("Total calls that emitted dex events:", lenSum)
+    assert(lenSum == successful.length);
 
-    // Start dex report
-    const swapExactSupplyCalls = dexTxs.filter((val) => val.method == 'addLiquidity')
-    console.log(swapExactSupplyCalls)
+    // dex module data
+    const swapExactSupplyCalls = dexTxs.filter((val) => val.method == 'swapWithExactSupply')
+    const swapExactTargetCalls = dexTxs.filter((val) => val.method == 'swapWithExactTarget')
+    const addLiquidityCalls = dexTxs.filter((val) => val.method == 'addLiquidity')
+    const removeLiquidityCalls = dexTxs.filter((val) => val.method == 'removeLiquidity')
+    const endProvisioningCall = dexTxs.filter((val) => val.method == 'endProvisioning')
+    const filteredDexCalls = [swapExactSupplyCalls, swapExactTargetCalls, addLiquidityCalls, removeLiquidityCalls, endProvisioningCall]
 
-    const aggDexSwap = aggregatedDexTxs.filter((val) => val.method == 'swapWithExactSupply');
-    //console.log(aggDexSwap);
+    const dexLen = filteredDexCalls.reduce(function (prev, curr) {
+        return prev + curr.length
+    }, 0)
+    assert(dexLen == dexTxs.length);
 
-    //const honzon = honzonTxs
-    //console.log(honzonTxs);
+    // Aggregated dex swap data
+    const aggSwapSupply = aggregatedDexTxs.filter((val) => val.method == 'swapWithExactSupply');
+    const aggSwapTarget = aggregatedDexTxs.filter((val) => val.method == 'swapWithExactTarget');
+    const filteredAggCalls = [aggSwapSupply, aggSwapTarget]
+
+    const aggLen = filteredAggCalls.reduce(function (prev, curr) {
+        return prev + curr.length
+    }, 0)
+    assert(aggLen == aggregatedDexTxs.length);
+
+    // Honzon data
+    const honzonSwapDex = honzonTxs.filter((val) => val.method == 'closeLoanHasDebitByDex')
+    assert(honzonSwapDex.length == honzonTxs.length)
+
+    // Utility batch
+    const batchAll = utilityTxs.filter((val) => val.method == 'batchAll');
+    const batch = utilityTxs.filter((val) => val.method == 'batch');
+
+    const batchSwapExactSupply = batch.filter((val) => val.params.calls[0].method == 'swapWithExactSupply')
+    const batchAddLiquidity = batch.filter((val) => val.params.calls[0].method == 'addLiquidity')
+    const batchAllSwapExactSupply = batchAll.filter((val) => val.params.calls[0].method == 'swapWithExactSupply')
+    const batchAllSwapExactTarget = batchAll.filter((val) => val.params.calls[0].method == 'swapWithExactTarget')
+    const batchAllAddLiquidity = batchAll.filter((val) => val.params.calls[0].method == 'addLiquidity')
+    const batchAllWithdrawDexShare = batchAll.filter((val) => val.params.calls[0].method == 'withdrawDexShare')
+    const batchAllRequestRedeem = batchAll.filter((val) => val.params.calls[0].method == 'requestRedeem')
+    const filteredBatch = [batchSwapExactSupply, batchAddLiquidity, batchAllSwapExactSupply, batchAllAddLiquidity, batchAllWithdrawDexShare, batchAllSwapExactTarget, batchAllRequestRedeem]
+
+    const batchLen = filteredBatch.reduce(function (prev, curr) {
+        return prev + curr.length
+    }, 0)
+    assert(batchLen == utilityTxs.length);
+
+    // Transaction Payment Calls data
+    const withFeePath = transactionFeeTxs.filter((val) => val.method == 'withFeePath');
+    const withFeeCurrency = transactionFeeTxs.filter((val) => val.method == 'withFeeCurrency');
+
+    // Both transactions are addLiquidity
+    const feeCurrencyAddLiquidity = withFeeCurrency;
+
+    const feePathCurrenciesTransfer = withFeePath.filter((val) => val.params.call.section == 'currencies' && val.params.call.method == 'transfer')
+    const feePathDexSwapSupply = withFeePath.filter((val) => val.params.call.section == 'dex' && val.params.call.method == 'swapWithExactSupply')
+    const feePathUtilities = withFeePath.filter((val) => val.params.call.section == 'utility')
+    // Three are swap with exact supply and then add Liquidity
+    const feePathUtilitiesSwap = feePathUtilities;
+
+    const filteredFeeTx = [feePathCurrenciesTransfer, feePathDexSwapSupply, feePathUtilitiesSwap, feeCurrencyAddLiquidity];
+
+    const feeTxLen = filteredFeeTx.reduce((prev, curr) => {
+        return prev + curr.length
+    }, 0)
+    assert(feeTxLen == transactionFeeTxs.length);
 
     /*
     const beforeBlock = 1638215
